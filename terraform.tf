c0ac8b72154df55519aad0e3d55879b4a9e22d4a
Radhika <poojariradhika1998@gmail.com>
2026-01-11 21:16:47 +0530

    Initial commit: Terraform EC2 setup

---
+++ terraform.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Lookup the latest Ubuntu 22.04 LTS AMI dynamically
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  count         = 3    
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform_Ubuntu_Demo"
+  }
+}
